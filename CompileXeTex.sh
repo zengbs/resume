d617e29572523d03793a638a1ec8531226a3c569
Po-Hsun Tseng <zengbs@gmail.com>
2022-03-03 19:42:15 +0800

    update

---
+++ CompileXeTex.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+#pdflatex -interaction=nonstopmode  ${filename}.tex
+#bibtex ${filename}.aux
+#pdflatex -interaction=nonstopmode ${filename}.tex
+#pdflatex -interaction=nonstopmode ${filename}.tex
+
+
+#rm -f  paper.aux paper.bbl paper.blg paper.log paper.out
+
+#xelatex -shell-escape  ${filename}.tex
+#bibtex    ${filename}.aux
+#xelatex -shell-escape ${filename}.tex
+#xelatex -shell-escape ${filename}.tex
+
+
+lualatex -shell-escape  ${filename}.tex
+bibtex    ${filename}.aux
+lualatex -shell-escape ${filename}.tex
+lualatex -shell-escape ${filename}.tex
